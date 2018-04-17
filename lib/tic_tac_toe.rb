d732b53622602fc2cf3f039f3a49bc2869a5f119
John Gratton <jgratton@gmail.com>
2018-04-17 19:43:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,8 +11,8 @@ class TicTacToe
   [2,4,6]
   ]
   
-  def initialize(board = Array.new(9, " "))
-    @board = board
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
   
   def board=(board)
